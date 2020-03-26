c58b3aa563a24f8dd66cb3d8d354847b27af9f12
matthew enbar <matthew.enbar@gmail.com>
2020-03-26 17:20:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,15 @@
-def is_prime(num)
-  (2..(num - 1)).each do |n|
-    return false if num % n == 0
+# Add  code here!
+def prime?(num)
+  n = 2
+  num = num.abs
+  if num == 1 || 0
+    return false
   end
-  true
+  if num == 2
+    return true
+  end
+    while n < num
+      return false if num % n == 0
+      n += 1
+    end
 end
