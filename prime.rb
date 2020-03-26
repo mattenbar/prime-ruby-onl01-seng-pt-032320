6c2e638d9b90a0e388dbf974a0796b0372191f90
matthew enbar <matthew.enbar@gmail.com>
2020-03-26 17:49:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(int)
     if int <= 1
       return false
     else
-        if (2...int).any? { |i| n % i  == 0}
+        if (2...int).any? { |i| int % i  == 0}
             return false
         else
             return true
