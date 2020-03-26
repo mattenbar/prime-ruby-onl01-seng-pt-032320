7b801b510a0ab66e375de45694c2621bc55f3702
matthew enbar <matthew.enbar@gmail.com>
2020-03-26 17:49:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(int)
+    if int <= 1
+      return false
+    else
+        if (2...int).any? { |i| int % i  == 0}
+            return false
+        else
+            return true
+        end
+    end
+end
