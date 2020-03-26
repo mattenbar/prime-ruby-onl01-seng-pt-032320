1e3348851c6f8f699df65fb363fe050780950e1f
matthew enbar <matthew.enbar@gmail.com>
2020-03-26 17:17:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,14 @@ def prime?(num)
   n = 1
   num = num.abs
   if num == 1 || 0
+    return false
+  end
+  if num == 2
     return true
   end
     while n < num
-      return true if num % n == 0
+      return false if num % n == 0
       n += 1
     end
-  false
+  true
 end
