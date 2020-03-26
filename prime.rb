7742fd1fb997d6e412d1c3c14f252479e7e06752
matthew enbar <matthew.enbar@gmail.com>
2020-03-26 17:06:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 # Add  code here!
-def prime?(int)
-  i = 2
-  while i < int
-    return false if int % i == 0
-    i += 1
+def prime?(num)
+  n = 2
+  while n < num
+    return false if num % n == 0
+    n += 1
   end
   true
-end
 end
