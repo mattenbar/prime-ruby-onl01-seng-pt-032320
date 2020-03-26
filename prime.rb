44da7dea30b50b2cf8f0aad1a510d262fc353ba9
matthew enbar <matthew.enbar@gmail.com>
2020-03-26 17:46:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,24 +1,11 @@
-# Add  code here!
-def prime?(num)
-  num = num.abs
-  count=0
-  if (num==0 || 1)
-  	return false
-  elsif (num==2)
-    return true
-  else
-  	i=2
-  	while(i<num)
-  		if (num%i==0)
-  			count+=1
-  		end
-  		i+=1
-  	end
-
-  end
-  	if count>1
-  		return false
-  	else
-  		return true
-  	end
+def prime?(n)
+    if n <= 1
+      return false
+    else
+        if (2...n).any? { |i| n % i  == 0}
+            return false
+        else
+            return true
+        end
+    end
 end
